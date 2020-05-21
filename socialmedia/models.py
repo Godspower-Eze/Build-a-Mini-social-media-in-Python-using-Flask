6ffe304697dfb27bff3deb672d1123184e8b0a0e
Godspower Eze <Godspowereze260@gmail.com>
2020-05-21 01:28:07 +0100

    Added models and the home route

---
+++ socialmedia/models.py
@@ -0,0 +1,25 @@
+from socialmedia import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(20), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    post_title = db.Column(db.String(50), nullable=False)
+    post_content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return self.post_title
---
+++ socialmedia/routes.py
@@ -0,0 +1,8 @@
+from socialmedia import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('base.html')
